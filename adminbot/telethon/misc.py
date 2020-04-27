6ac39bb4a6e403468576631762640a96c4eaf604
Arne Beer <contact@arne.beer>
2020-04-27 20:21:53 +0200

    Add default feature request message

--- adminbot/telethon/misc.py
+++ adminbot/telethon/misc.py
@@ -21,3 +21,23 @@ async def print_id(event):
     message = f"(Bot) Chat type: {peer_type}, chat id: {chat_id}"
 
     await event.respond(message)
+
+
+@bot.on(
+    events.NewMessage(
+        pattern="\\\\default",
+        outgoing=True,
+        forwards=False,
+        from_users=config["bot"]["admin"],
+    )
+)
+async def default_feature_request(event):
+    """Print the current chat id and type for debugging."""
+    chat_id, peer_type = get_peer_information(event.message.to_id)
+    message = (
+        "This has already been proposed, but it's not implemented yet and I don't know when (if ever) it will be implemented.\n"
+        "Frankly, this is no feature that I need myself. Thereby, I'll most likely not build it myself.\n\n"
+        "In case somebody comes up with some nice concept and code, feel free to create a pull request. I'll then consider adding it to the pollbot :)"
+    )
+
+    await event.edit(message)

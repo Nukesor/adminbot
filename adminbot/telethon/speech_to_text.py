4b2e130d6fd60b75f2d32fdc4ce592a345f54a99
Arne Beer <contact@arne.beer>
2024-05-16 22:37:10 +0200

    change: Send my own speech-to-text messages as reply

--- adminbot/telethon/speech_to_text.py
+++ adminbot/telethon/speech_to_text.py
@@ -38,7 +38,13 @@ async def speech_to_text(event):
     print(f"Detection finished, detected the following text:\n{output}")
     response = f"Speech-to-text detection:\n\n{output}"
 
-    await event.client.send_message("me", response)
+    # Send speech-to-text message in the same chat for my own messages.
+    # However, send speech-to-text message for other people to my own saved messages folder.
+    # I don't want to scare them off.
+    if message.from_id == bot.get_me().id:
+        await event.reply(response)
+    else:
+        await event.client.send_message("me", response)
 
 
 async def detect_text(message) -> str | None:
